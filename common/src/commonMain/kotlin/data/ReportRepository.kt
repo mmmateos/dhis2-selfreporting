c4ed70922a873bae7595aece97c03b5e90c7c6cf
andresmr <andres@andresmr.com>
2022-10-26 13:48:49 +0200

    send report id

--- common/src/commonMain/kotlin/data/ReportRepository.kt
+++ common/src/commonMain/kotlin/data/ReportRepository.kt
@@ -3,14 +3,37 @@ package data
 import io.ktor.client.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
+import io.ktor.http.*
 import model.Report
 
 class ReportRepository {
     suspend fun sendReport(report: Report): Boolean {
         val client = HttpClient()
-        val response: HttpResponse = client.get("https://ktor.io/")
-        println(response.status)
+        /**
+         * "{\n" +
+        "    \"id\": \"dwWgzxGz0S4\",\n" +
+        "    \"systolic\": \"306\",\n" +
+        "    \"diastolic\": \"306\",\n" +
+        "    \"pulse\": \"306\",\n" +
+        "    \"weight\": \"306\"\n" +
+        "}"
+         */
+        val body ="{\n" +
+                "    \"id\": \"${report.id}\",\n" +
+                "    \"systolic\": \"${report.systolic}\",\n" +
+                "    \"diastolic\": \"${report.diastolic}\",\n" +
+                "    \"pulse\": \"${report.pulse}\",\n" +
+                "    \"weight\": \"${report.weight}\"\n" +
+                "}"
+        println(body)
+        val response: HttpResponse = client.post("http://172.104.146.122:8080/api/self-reporting/vital-signs"){
+            contentType(ContentType.Application.Json)
+
+            setBody(body)
+        }
+        println("Response Status $response.status")
+        val result = response.status == HttpStatusCode.OK
         client.close()
-        return true
+        return result
     }
 }
--- common/src/commonMain/kotlin/model/Report.kt
+++ common/src/commonMain/kotlin/model/Report.kt
@@ -1,6 +1,7 @@
 package model
 
 data class Report(
+    val id: String,
     val diastolic: Int,
     val systolic: Int,
     val pulse: Int,
